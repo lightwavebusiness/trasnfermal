112f5c28c6e5347fda2e0f770992d8ed73f128f1
lightwavebusiness <leon@l-w-b.co>
2014-02-17 22:31:41 +0000

    Tesr

---
+++ 2014/Controllers/ApplicationController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace _2014.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2014/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_2014.Startup))]
+
+namespace _2014
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
